34cf273eabe976167a3c170338006a39fb384141
heebong <heebg2@naver.com>
2019-11-26 15:58:55 +0900

    initial commit

---
+++ src/main/java/com/h3/reservation/ReservationApplication.java
@@ -0,0 +1,13 @@
+package com.h3.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationApplication.class, args);
+    }
+
+}
