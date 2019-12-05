9a03417ae1aaab020da9a9f0b8e8e2029c296230
choihz <choixhz@gmail.com>
2019-12-05 14:28:07 +0900

    feat: 예약 조회 모달에 시간 선택 기능 추가

--- src/main/java/com/h3/reservation/slack/service/SlackService.java
+++ src/main/java/com/h3/reservation/slack/service/SlackService.java
@@ -8,10 +8,14 @@ import com.h3.reservation.slack.dto.response.InitialResponse;
 import com.h3.reservation.slack.dto.response.RetrieveResponse;
 import com.h3.reservation.slack.fragment.block.ActionsBlock;
 import com.h3.reservation.slack.fragment.block.InputBlock;
+import com.h3.reservation.slack.fragment.block.SectionBlock;
+import com.h3.reservation.slack.fragment.composition.Option;
+import com.h3.reservation.slack.fragment.composition.text.MrkdwnText;
+import com.h3.reservation.slack.fragment.composition.text.PlainText;
 import com.h3.reservation.slack.fragment.element.ButtonElement;
 import com.h3.reservation.slack.fragment.element.DatepickerElement;
 import com.h3.reservation.slack.fragment.element.Element;
-import com.h3.reservation.slack.fragment.composition.text.PlainText;
+import com.h3.reservation.slack.fragment.element.StaticSelectElement;
 import com.h3.reservation.slack.fragment.view.ModalView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +25,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -49,11 +54,11 @@ public class SlackService {
     public void viewModal(BlockActionRequest dto) {
         InitMenuType type = InitMenuType.valueOf(dto.getAction_id().toUpperCase());
         String postUrl = "https://slack.com/api/views.open";
-        Object response =  new PlainText(dto.getAction_id());
-        if (InitMenuType.RETRIEVE == type) {
+        Object response = new PlainText(dto.getAction_id());
+        if (InitMenuType.RETRIEVE.equals(type)) {
             response = generateRetrieveResponse(dto.getTrigger_id());
         }
-        send(postUrl,response);
+        send(postUrl, response);
     }
 
     private void send(String url, Object dto) {
@@ -89,11 +94,57 @@ public class SlackService {
         DatepickerElement datePicker = new DatepickerElement("retrieve_datepicker");
 
         ModalView modalView = new ModalView(
-            new PlainText("예약 조회하기"),
+            new PlainText("조회하기"),
             new PlainText("조회"),
             new PlainText("취소"),
-            Collections.singletonList(new InputBlock(new PlainText("조회할 날짜를 선택하세요. \uD83D\uDE03"), datePicker))
+            Arrays.asList(
+                new InputBlock(new PlainText("조회할 날짜를 선택하세요."), datePicker),
+                new SectionBlock(new MrkdwnText("*시작 시간을 선택하세요*")),
+                generateTimePicker(
+                    "retrieve_start_time", "retrieve_start_minute", 10, 0
+                ),
+                new SectionBlock(new MrkdwnText("*종료 시간을 선택하세요*")),
+                generateTimePicker(
+                    "retrieve_end_time", "retrieve_end_minute", 21, 50
+                )
+            )
         );
         return new RetrieveResponse(trigger_id, modalView);
+    }
+
+    private ActionsBlock generateTimePicker(String timeActionId, String minuteActionId,
+                                            int initialTime, int initialMinute) {
+        return new ActionsBlock(
+            Arrays.asList(
+                new StaticSelectElement(
+                    new PlainText("시"),
+                    timeActionId,
+                    new Option(new PlainText(initialTime + "시"), String.valueOf(initialTime)),
+                    generateTimeSelect()
+                ),
+                new StaticSelectElement(
+                    new PlainText("분"),
+                    minuteActionId,
+                    new Option(new PlainText(initialMinute + "분"), String.valueOf(initialMinute)),
+                    generateMinuteSelect()
+                )
+            )
+        );
+    }
+
+    private List<Option> generateTimeSelect() {
+        List<Option> options = new ArrayList<>();
+        for (int i = 10; i <= 21; i++) {
+            options.add(new Option(new PlainText(i + "시"), String.valueOf(i)));
+        }
+        return options;
+    }
+
+    private List<Option> generateMinuteSelect() {
+        List<Option> options = new ArrayList<>();
+        for (int i = 0; i <= 50; i += 10) {
+            options.add(new Option(new PlainText(i + "분"), String.valueOf(i)));
+        }
+        return options;
     }
 }
