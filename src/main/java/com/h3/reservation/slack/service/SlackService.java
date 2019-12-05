c97d3fa6da048bf9af63b2da894c695b12bc8b9e
choihz <choixhz@gmail.com>
2019-12-05 15:35:22 +0900

    refactor: ResponseFactory 생성

--- src/main/java/com/h3/reservation/slack/service/SlackService.java
+++ src/main/java/com/h3/reservation/slack/service/SlackService.java
@@ -4,19 +4,9 @@ import com.h3.reservation.slack.InitMenuType;
 import com.h3.reservation.slack.dto.request.BlockActionRequest;
 import com.h3.reservation.slack.dto.request.EventCallbackRequest;
 import com.h3.reservation.slack.dto.request.VerificationRequest;
-import com.h3.reservation.slack.dto.response.InitialResponse;
-import com.h3.reservation.slack.dto.response.RetrieveResponse;
-import com.h3.reservation.slack.fragment.block.ActionsBlock;
-import com.h3.reservation.slack.fragment.block.InputBlock;
-import com.h3.reservation.slack.fragment.block.SectionBlock;
-import com.h3.reservation.slack.fragment.composition.Option;
-import com.h3.reservation.slack.fragment.composition.text.MrkdwnText;
+import com.h3.reservation.slack.dto.response.factory.InitResponseFactory;
+import com.h3.reservation.slack.dto.response.factory.RetrieveResponseFactory;
 import com.h3.reservation.slack.fragment.composition.text.PlainText;
-import com.h3.reservation.slack.fragment.element.ButtonElement;
-import com.h3.reservation.slack.fragment.element.DatepickerElement;
-import com.h3.reservation.slack.fragment.element.Element;
-import com.h3.reservation.slack.fragment.element.StaticSelectElement;
-import com.h3.reservation.slack.fragment.view.ModalView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -24,11 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * @author heebg
@@ -48,7 +33,7 @@ public class SlackService {
 
     public void eventCallBack(EventCallbackRequest dto) {
         String postUrl = "https://slack.com/api/chat.postMessage";
-        send(postUrl, generateInitResponse(dto.getChannel()));
+        send(postUrl, InitResponseFactory.of(dto.getChannel()));
     }
 
     public void viewModal(BlockActionRequest dto) {
@@ -56,7 +41,7 @@ public class SlackService {
         String postUrl = "https://slack.com/api/views.open";
         Object response = new PlainText(dto.getAction_id());
         if (InitMenuType.RETRIEVE.equals(type)) {
-            response = generateRetrieveResponse(dto.getTrigger_id());
+            response = RetrieveResponseFactory.of(dto.getTrigger_id());
         }
         send(postUrl, response);
     }
@@ -75,76 +60,5 @@ public class SlackService {
             .exchange().block().bodyToMono(String.class)
             .block();
         logger.debug("webclient response 응답 : {}", response);
-    }
-
-    private InitialResponse generateInitResponse(String channel) {
-        List<Element> elements = Arrays.asList(
-            new ButtonElement(new PlainText(":spiral_calendar_pad: 전체 조회"), "retrieve"),
-            new ButtonElement(new PlainText(":pushpin: 회의실 예약"), "reserve"),
-            new ButtonElement(new PlainText(":file_folder: 예약 확인"), "confirm"),
-            new ButtonElement(new PlainText(":memo: 예약 변경"), "change"),
-            new ButtonElement(new PlainText(":scissors: 예약 취소"), "cancel"));
-
-        List<ActionsBlock> blocks = Collections.singletonList(new ActionsBlock(elements));
-
-        return new InitialResponse(channel, blocks);
-    }
-
-    private RetrieveResponse generateRetrieveResponse(String trigger_id) {
-        DatepickerElement datePicker = new DatepickerElement("retrieve_datepicker");
-
-        ModalView modalView = new ModalView(
-            new PlainText("조회하기"),
-            new PlainText("조회"),
-            new PlainText("취소"),
-            Arrays.asList(
-                new InputBlock(new PlainText("조회할 날짜를 선택하세요."), datePicker),
-                new SectionBlock(new MrkdwnText("*시작 시간을 선택하세요*")),
-                generateTimePicker(
-                    "retrieve_start_time", "retrieve_start_minute", 10, 0
-                ),
-                new SectionBlock(new MrkdwnText("*종료 시간을 선택하세요*")),
-                generateTimePicker(
-                    "retrieve_end_time", "retrieve_end_minute", 21, 50
-                )
-            )
-        );
-        return new RetrieveResponse(trigger_id, modalView);
-    }
-
-    private ActionsBlock generateTimePicker(String timeActionId, String minuteActionId,
-                                            int initialTime, int initialMinute) {
-        return new ActionsBlock(
-            Arrays.asList(
-                new StaticSelectElement(
-                    new PlainText("시"),
-                    timeActionId,
-                    new Option(new PlainText(initialTime + "시"), String.valueOf(initialTime)),
-                    generateTimeSelect()
-                ),
-                new StaticSelectElement(
-                    new PlainText("분"),
-                    minuteActionId,
-                    new Option(new PlainText(initialMinute + "분"), String.valueOf(initialMinute)),
-                    generateMinuteSelect()
-                )
-            )
-        );
-    }
-
-    private List<Option> generateTimeSelect() {
-        List<Option> options = new ArrayList<>();
-        for (int i = 10; i <= 21; i++) {
-            options.add(new Option(new PlainText(i + "시"), String.valueOf(i)));
-        }
-        return options;
-    }
-
-    private List<Option> generateMinuteSelect() {
-        List<Option> options = new ArrayList<>();
-        for (int i = 0; i <= 50; i += 10) {
-            options.add(new Option(new PlainText(i + "분"), String.valueOf(i)));
-        }
-        return options;
     }
 }
