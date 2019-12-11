899639becbe21541ed27a8d753791fcb8039bb73
heebong <heebg2@naver.com>
2019-12-11 13:24:10 +0900

    test: 테스트 코드 리팩토링

--- src/test/java/com/h3/reservation/slackcalendar/service/SlackCalendarServiceTest.java
+++ src/test/java/com/h3/reservation/slackcalendar/service/SlackCalendarServiceTest.java
@@ -4,9 +4,9 @@ import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.EventDateTime;
 import com.h3.reservation.calendar.CalendarService;
+import com.h3.reservation.slackcalendar.dto.SlackCalendarEvent;
 import com.h3.reservation.slackcalendar.dto.SlackCalendarRetrieveRequest;
 import com.h3.reservation.slackcalendar.dto.SlackCalendarRetrieveResponse;
-import com.h3.reservation.slackcalendar.dto.SlackCalendarEvent;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -35,14 +35,16 @@ class SlackCalendarServiceTest {
 
     @Test
     void retrieve() {
+        String date = "2019-12-10";
+
         List<Event> googleEvents = new ArrayList<>();
-        googleEvents.add(createEvent("회의실1/희봉/프로젝트", "2019-12-10T10:30:00.000+09:00", "2019-12-10T12:00:00.000+09:00"));
-        googleEvents.add(createEvent("회의실1/코니/회고", "2019-12-10T13:00:00.000+09:00", "2019-12-10T15:30:00.000+09:00"));
-        googleEvents.add(createEvent("회의실2/도넛/굴러간다", "2019-12-10T11:00:00.000+09:00", "2019-12-10T12:00:00.000+09:00"));
-        googleEvents.add(createEvent("회의실3/버디/회의", "2019-12-10T17:00:00.000+09:00", "2019-12-10T18:00:00.000+09:00"));
+        googleEvents.add(createEvent("회의실1/희봉/프로젝트", date, "10:30", "12:00"));
+        googleEvents.add(createEvent("회의실1/코니/회고", date, "13:00", "15:30"));
+        googleEvents.add(createEvent("회의실2/도넛/굴러간다", date, "11:00", "12:00"));
+        googleEvents.add(createEvent("회의실3/버디/회의", date, "17:00", "18:00"));
 
         when(calendarService.findReservation(any(), any())).thenReturn(googleEvents);
-        String date = "2019-12-10";
+
         String startTime = "10:00";
         String endTime = "18:00";
         SlackCalendarRetrieveRequest request = new SlackCalendarRetrieveRequest(date, startTime, endTime);
@@ -57,10 +59,28 @@ class SlackCalendarServiceTest {
         assertEquals(response.getSlackCalendarEvents(), slackCalendarEvents);
     }
 
-    private Event createEvent(String summary, String startTime, String endTime) {
+    /**
+     *
+     * @param summary       room/booker/purpose
+     * @param date          yyyy-MM-dd
+     * @param startTime     hh:mm
+     * @param endTime       hh:mm
+     * @return
+     */
+    private Event createEvent(String summary, String date, String startTime, String endTime) {
         return new Event()
             .setSummary(summary)
-            .setStart(new EventDateTime().setDateTime(DateTime.parseRfc3339(startTime)))
-            .setEnd(new EventDateTime().setDateTime(DateTime.parseRfc3339(endTime)));
+            .setStart(new EventDateTime().setDateTime(DateTime.parseRfc3339(generateDateTime(date, startTime))))
+            .setEnd(new EventDateTime().setDateTime(DateTime.parseRfc3339(generateDateTime(date, endTime))));
+    }
+
+    /**
+     *
+     * @param date      yyyy-MM-dd
+     * @param time      hh:mm
+     * @return
+     */
+    private String generateDateTime(String date, String time) {
+        return date + "T" + time + ":00.000+09:00";
     }
 }
